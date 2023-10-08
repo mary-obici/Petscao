3110e390d4c76edb44352ba3c7f7f181bbb65942
Luiz Henrique <lhchoinski04@gmail.com>
2023-10-08 15:38:28 -0300

    Organizacao das classes e revisao de codigo

--- Models/ProductCategory.cs
+++
@@ -1,9 +0,0 @@
-namespace Petscao.Models;
-
-public class ProductCategory
-{
-    public int ProductCategoryId { get; set; }
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-    public DateTime CreatedAt { get; set; }
-}
